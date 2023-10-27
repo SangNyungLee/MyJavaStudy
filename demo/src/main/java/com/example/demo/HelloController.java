fae421ddfc7b3f71a0dd4fb3402f50a6758c5363
SangNyungLee <lsy6747@gmail.com>
2023-10-27 17:45:05 +0900

    java

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String getHi(Model model){
+        model.addAttribute("msg", "hi");
+        return "hi";
+    }
+}
