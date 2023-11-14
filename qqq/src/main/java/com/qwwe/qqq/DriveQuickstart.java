e2982c05dea4eaf59de043b53feb1e69299ccd7f
SangNyungLee <lsy6747@gmail.com>
2023-11-14 20:56:34 +0900

    3_project

---
+++ qqq/src/main/java/com/qwwe/qqq/DriveQuickstart.java
@@ -0,0 +1,123 @@
+package com.qwwe.qqq;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+/* class to demonstrate use of Drive files list API */
+public class DriveQuickstart {
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException {
+        // Load client secrets.
+        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        //returns an authorized Credential object.
+        return credential;
+    }
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+
+        List<File> files = result.getFiles();
+        if (files == null || files.isEmpty()) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                if(file.getName().equals("test.txt")){
+                    System.out.println("test 파일 찾음");
+//                    DownloadFile.downloadFile(file.getName());
+                    System.out.println(file.getId());
+                }else{
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+                }
+            }
+        }
+        //파일 업로드
+//        System.out.println("\n\n 파일 업로드 시작..");
+//        File fileMetaData = new File();
+//        fileMetaData.setName("uploadtest.txt"); //업로드 파일 이름
+//        java.io.File f = new java.io.File("C:\\Users\\SangNyung\\Desktop\\ee\\uploadtest.txt");
+//        FileContent fileContent = new FileContent("text/plain", f);
+//
+//        service.files().create(fileMetaData, fileContent).setFields("id").execute();
+        UploadBasic.uploadBasic();
+//        try {
+//            String folderId = CreateFolder.createFolder();
+//            String upload = UploadBasic.uploadBasic();
+//            System.out.println("Created folder with ID: " + folderId);
+//            System.out.println("Create image:" + upload);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
---
+++ qqq/src/test/java/com/qwwe/qqq/QqqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwwe.qqq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
